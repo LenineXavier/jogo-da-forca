554665ab832ad6f84f808485be70c46436ee0f70
Lenine Xavier <leninecxavier@gmail.com>
2022-01-26 15:37:47 -0300

    inicio codigo

--- scripts/forca.js
+++ scripts/forca.js
@@ -1,13 +1,38 @@
+const arrPalavras = ["computador", "escola","rota", "javascript", "monitor","falso","verdadeiro","carne","erros","ironhack","podcast","livros","gato","teclado","janela","porta","palavras","praia", "mar", "pedreira", "arrocha", "banguela", "desafio", "internet","palindromo", "vassalo","fantasia"];
+
 let tentativas = 6;
 let acertos = 0;
+let imagem = 0;
 
-const arrPalavras = ["computador", "escola","rota", "javascript", "monitor","falso","verdadeiro","carne","erros","ironhack","podcast","livros","gato","teclado","janela","porta","palavras","praia", "mar", "pedreira", "arrocha", "banguela", "desafio", "internet","palindromo", "vassalo","fantasia"];
     
 
-function pegarPalavra (){
+
     const palavra = arrPalavras[Math.floor(Math.random() * arrPalavras.length)];
+
+
+
+
+    for( i = 0 ; i < arrPalavras.length ; i++ ){
+
+        let span = document.createElement("span");
+    span.setAttribute('id', i);
+        let div = document.getElementById("palavra");
+        div.appendChild(span);
+        
+    }
+    
+    let alfabeto = "abcdefghijklmnopqrstuvwxyz";
+    let letras = alfabeto.split("");
+
+for (i = 0; i < letras.length; i++) {
+    let botao = document.createElement("button");
+    let letra = document.createTextNode(letras[i]);
+    
+    botao.appendChild(letra);
+    botao.setAttribute('onclick', 'escolheLetra(\''+letras[i]+'\')');
+    botao.setAttribute('id', letras[i]);
+
+    let div = document.getElementById("letras");
+    div.appendChild(botao);
 }
 
-function mostrarPalavra (){
-    
-}
